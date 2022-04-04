70fa50ef8f4988c65e01a31b1df2488d13acde14
andgarzonmal <andgarzonmal@unal.edu.co>
2022-04-04 10:55:31 -0500

    🪛 Fix tab accesibility

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const menu = document.querySelector("#menu");
 const navIcons = document.querySelector(".nav-iconos");
-const listComponent = document.querySelector(".header")
-
+const listComponent = document.querySelector(".header");
+const navElements =document.querySelectorAll(".toggle")
 
 
 function displayMenu() {
   listComponent.classList.add("display-my-menu");
   listComponent.classList.remove("header");
+  document. getElementById('.menu'). style. display = "none";
+  navIcons. style. display = "contents";
 }
 
+function addLines() {
+  for(let i=0; i<navElements.length; i++) {
+    navElements[i].insertAdjacentHTML("afterend", '<hr/>');
+    }
+}
 
-menu.addEventListener("click", displayMenu)
+menu.addEventListener("click", () => {
+  displayMenu();
+  addLines();
+});
 
