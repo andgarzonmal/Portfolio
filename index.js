d32a3b5bd624e3ae17b4f86e4241fcabfc343034
andgarzonmal <andgarzonmal@unal.edu.co>
2022-04-04 18:12:06 -0500

    🪛 ADD linters for js

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-const menu = document.querySelector("#menu");
-const navIcons = document.querySelector(".nav-iconos");
-const listComponent = document.querySelector(".header");
-const navElements =document.querySelectorAll(".toggle")
+const menu = document.querySelector('#menu');
+const navIcons = document.querySelector('.nav-iconos');
+const listComponent = document.querySelector('.header');
+const navElements = document.querySelectorAll('.toggle');
 const closeMenu = document.querySelector('.clear');
 
-navElements[0].insertAdjacentHTML("afterend", '<div class="pop-menu-line"></div>');
-navElements[1].insertAdjacentHTML("afterend", '<div class="pop-menu-line"></div>');
-navElements[2].insertAdjacentHTML("afterend", '<div class="pop-menu-line"></div>');
+navElements[0].insertAdjacentHTML('afterend', '<div class="pop-menu-line"></div>');
+navElements[1].insertAdjacentHTML('afterend', '<div class="pop-menu-line"></div>');
+navElements[2].insertAdjacentHTML('afterend', '<div class="pop-menu-line"></div>');
 
 function displayMenu() {
-  listComponent.classList.add("display-my-menu");
-  listComponent.classList.remove("header");
-  document. getElementById('.menu'). style. display = "none";
-  navIcons. style. display = "contents";
+  listComponent.classList.add('display-my-menu');
+  listComponent.classList.remove('header');
+  document.getElementById('.menu').style.display = 'none';
+  navIcons.style.display = 'contents';
 }
 
 function hideMenu() {
-  listComponent.classList.remove("display-my-menu");
-  listComponent.classList.add("header");
+  listComponent.classList.remove('display-my-menu');
+  listComponent.classList.add('header');
 }
 
+navElements[0].addEventListener('click', hideMenu);
+navElements[1].addEventListener('click', hideMenu);
+navElements[2].addEventListener('click', hideMenu);
 
-navElements[0].addEventListener("click", hideMenu)
-navElements[1].addEventListener("click", hideMenu)
-navElements[2].addEventListener("click", hideMenu)
-
-
-menu.addEventListener("click", displayMenu);
-
+menu.addEventListener('click', displayMenu);
 
 closeMenu.addEventListener('click', hideMenu);
