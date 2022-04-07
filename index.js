61b9c5b9bcec825d6cdde4b55a5872cc50eb2bf9
andgarzonmal <andgarzonmal@unal.edu.co>
2022-04-07 08:33:30 -0500

    ✔️ Verify objects modifiy cards info correctly

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ closeMenu.addEventListener('click', hideMenu);
 // cards objects
 
 const multyPostSection = {
-    title: "kjdfbiskjfksabjdkfjsdkfjbskb",
+    title: "kjdf",
     tech: ["Css", "html", "bootstrap", "Ruby"],
     image: "./images/Stories.png",
     descriptionWhenOpen: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
